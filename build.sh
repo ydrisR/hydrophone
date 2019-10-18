bbd908733001609e957f31f6c37c068a476d9801
Ydris Rebibane <ydris@diabeloop.fr>
2019-10-18 13:58:38 +0200

    Update build.sh
    Co-Authored-By: Fabrice Sougey <33152621+BigFab@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ VERSION_FULL_COMMIT=$(git rev-parse HEAD)
 GO_COMMON_PATH="github.com/tidepool-org/go-common"
 	
 echo "Build hydrophone $VERSION_BASE+$VERSION_FULL_COMMIT"
+go mod tidy
 go build -ldflags "-X $GO_COMMON_PATH/clients/version.ReleaseNumber=$VERSION_BASE \
     -X $GO_COMMON_PATH/clients/version.FullCommit=$VERSION_FULL_COMMIT \
     -X $GO_COMMON_PATH/clients/version.ShortCommit=$VERSION_SHORT_COMMIT" \
